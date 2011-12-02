009c4cd0d9cd44efeb2a4158264481f2a5a632b1
hsiayong <hsiayong@96a9805f-92f8-c556-2e62-a92eab900872>
2011-12-02 14:31:02 +0000

    增加测试用例

---
+++ trunk/project/soquickClient/src/test/java/com/apusic/soquick/BaseTestCase.java
@@ -0,0 +1,8 @@
+package com.apusic.soquick;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+public class BaseTestCase {
+	protected static final Log logger = LogFactory.getLog(BaseTestCase.class);
+}
