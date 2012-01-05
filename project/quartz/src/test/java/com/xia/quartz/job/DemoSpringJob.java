5195a068126fefc160288077cdbd884e6029135a
hsiayong <hsiayong@96a9805f-92f8-c556-2e62-a92eab900872>
2012-01-05 13:09:13 +0000

    增加事件，增加错误数

--- project/quartz/src/test/java/com/xia/quartz/job/DemoSpringJob.java
+++ project/quartz/src/test/java/com/xia/quartz/job/DemoSpringJob.java
@@ -2,14 +2,12 @@ package com.xia.quartz.job;
 
 import org.quartz.JobDataMap;
 
-import com.xia.quartz.model.JobEntity;
-
 public class DemoSpringJob  extends AbstractJob {
 
 	@Override
 	public void execute(JobDataMap jobDataMap) throws Exception {
-		JobEntity job = getJobEntityService().findJobEntityByJobName("Demo任务_1325661555923");
-		logger.debug("Finded:"+job);
+		//JobEntity job = getJobEntityService().findJobEntityByJobName("Demo任务_1325661555923");
+		logger.debug("DemoSpringJob Runned:");
 	}
 
 }
