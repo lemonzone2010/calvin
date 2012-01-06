ffea920aee4188e41e26157f2b23594b87f99283
hsiayong <hsiayong@96a9805f-92f8-c556-2e62-a92eab900872>
2012-01-06 07:30:52 +0000

    Spring invoker实现

--- trunk/project/quartz/src/test/java/com/xia/quartz/job/DemoSpringJob.java
+++ trunk/project/quartz/src/test/java/com/xia/quartz/job/DemoSpringJob.java
@@ -1,9 +1,13 @@
 package com.xia.quartz.job;
 
 import org.quartz.JobDataMap;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.xia.quartz.service.JobEntityService;
 
 public class DemoSpringJob  extends AbstractJob {
-
+	@Autowired
+	JobEntityService jobEntityService;
 	@Override
 	public void execute(JobDataMap jobDataMap) throws Exception {
 		//JobEntity job = getJobEntityService().findJobEntityByJobName("Demo任务_1325661555923");
