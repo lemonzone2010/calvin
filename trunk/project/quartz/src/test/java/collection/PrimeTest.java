e3ad11c48f042936b321188661d3522956b4dcde
hsiayong <hsiayong@96a9805f-92f8-c556-2e62-a92eab900872>
2012-02-20 09:46:10 +0000

    update

---
+++ trunk/project/quartz/src/test/java/collection/PrimeTest.java
@@ -0,0 +1,27 @@
+package collection;
+
+public class PrimeTest {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		int N = 15;
+		for (int i = 1; i < N; i++) {
+			if (isPrime(i)) {
+				System.out.println(i);
+			}
+		}
+
+	}
+
+	private static boolean isPrime(final int i) {
+		for (int j = 2; j < i; j++) {
+			if (i % j == 0) {
+				return false;
+			}
+		}
+		return true;
+	}
+
+}
