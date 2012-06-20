4e06b810d058075e549d008dc4a76dd7733a7ef9
hsiayong <hsiayong@gmail.com>
2012-06-20 06:35:46 +0000

    fix cas auth problem

--- project/beer/application/iam-service/src/main/java/com/apusic/ebiz/iam/security/access/expression/InvocationSecurityMetadataSourceService.java
+++ project/beer/application/iam-service/src/main/java/com/apusic/ebiz/iam/security/access/expression/InvocationSecurityMetadataSourceService.java
@@ -12,7 +12,6 @@ import javax.servlet.http.HttpServletRequest;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
-import org.springframework.beans.factory.InitializingBean;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.ConfigAttribute;
 import org.springframework.security.access.SecurityConfig;
@@ -21,8 +20,11 @@ import org.springframework.security.web.access.intercept.FilterInvocationSecurit
 import org.springframework.security.web.util.AntPathRequestMatcher;
 import org.springframework.security.web.util.RequestMatcher;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Propagation;
-import org.springframework.transaction.annotation.Transactional;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.TransactionDefinition;
+import org.springframework.transaction.TransactionStatus;
+import org.springframework.transaction.support.TransactionCallbackWithoutResult;
+import org.springframework.transaction.support.TransactionTemplate;
 
 import com.apusic.ebiz.model.user.Resource;
 import com.apusic.ebiz.model.user.Role;
@@ -30,67 +32,75 @@ import com.apusic.ebiz.smartorg.service.ResourceService;
 import com.apusic.ebiz.smartorg.service.RoleResourceService;
 
 @Service("iam_SecurityMetadataSource")
-public class InvocationSecurityMetadataSourceService implements
-		FilterInvocationSecurityMetadataSource ,InitializingBean{
+public class InvocationSecurityMetadataSourceService implements FilterInvocationSecurityMetadataSource {
 
 	private static final Log logger = LogFactory.getLog(InvocationSecurityMetadataSourceService.class);
 
 	@Autowired
-    private ResourceService resourceService;
+	private ResourceService resourceService;
 
 	@Autowired
 	private RoleResourceService roleResourceService;
 
+	private static Map<String, Collection<ConfigAttribute>> resourceMap = null;
+	private TransactionTemplate transactionTemplate;
 
-    private static Map<String, Collection<ConfigAttribute>> resourceMap = null;
+	@Autowired
+	public void setTransactionManager(PlatformTransactionManager transactionManager) {
+		this.transactionTemplate = new TransactionTemplate(transactionManager);
+	}
 
-    //@PostConstruct
-    @Transactional(readOnly=true,propagation=Propagation.REQUIRED)
-    public void loadResourceDefine()throws Exception  {
-        this.resourceMap = new HashMap<String, Collection<ConfigAttribute>>();
-        //通过数据库中的信息设置，resouce和role
-      for(Resource item : this.resourceService.getAllResources()){
-    	  List<Role> roles = this.roleResourceService.findRolesbyResourceId(item.getId());
-    	  if(roles!=null && roles.size() > 0){
-    		  Collection<ConfigAttribute> atts = new ArrayList<ConfigAttribute>();
-    		  for(Role role : roles){
-    			  atts.add(new SecurityConfig(role.getName()));
-    		  }
-    		  if(logger.isDebugEnabled()){
-    			  logger.debug("resouce：["+item.getResName()+"]拥有的role有："+atts);
-    		  }
-    		 
-    		  this.resourceMap.put("/"+ item.getResContext()+item.getResValue(), atts);
-    	  }
-      }
-    }
+	@PostConstruct
+	public void inits() {
+		transactionTemplate.setPropagationBehavior(TransactionDefinition.PROPAGATION_REQUIRED);
+		transactionTemplate.execute(new TransactionCallbackWithoutResult() {
+					@Override
+					protected void doInTransactionWithoutResult(TransactionStatus arg0) {
+						loadResourceDefine();
+					}
+				});
+	}
 
-    public Collection<ConfigAttribute> getAttributes(Object object) throws IllegalArgumentException {
-    	FilterInvocation filterInvocation = (FilterInvocation) object;
-    	HttpServletRequest request = filterInvocation.getHttpRequest();
+	public void loadResourceDefine() {
+		resourceMap = new HashMap<String, Collection<ConfigAttribute>>();
+		// 通过数据库中的信息设置，resouce和role
+		for (Resource item : resourceService.getAllResources()) {
+			List<Role> roles = roleResourceService.findRolesbyResourceId(item.getId());
+			if (roles != null && roles.size() > 0) {
+				Collection<ConfigAttribute> atts = new ArrayList<ConfigAttribute>();
+				for (Role role : roles) {
+					atts.add(new SecurityConfig(role.getName()));
+				}
+				if (logger.isDebugEnabled()) {
+					logger.debug("resouce：[" + item.getResName() + "]拥有的role有：" + atts);
+				}
 
-        Iterator<String> ite = resourceMap.keySet().iterator();
-        while (ite.hasNext()) {
-            String resURL = ite.next();
-            RequestMatcher requestMatcher = new AntPathRequestMatcher(resURL);
-            if (requestMatcher.matches(request)) {
-                Collection<ConfigAttribute> returnCollection = resourceMap.get(resURL);
-                return returnCollection;
-            }
-        }
-        return null;
-    }
+				resourceMap.put("/" + item.getResContext() + item.getResValue(), atts);
+			}
+		}
+	}
 
-    public boolean supports(Class<?> clazz) {
-        return true;
-    }
+	public Collection<ConfigAttribute> getAttributes(Object object) throws IllegalArgumentException {
+		FilterInvocation filterInvocation = (FilterInvocation) object;
+		HttpServletRequest request = filterInvocation.getHttpRequest();
 
-    public Collection<ConfigAttribute> getAllConfigAttributes() {
-        return null;
-    }
+		Iterator<String> ite = resourceMap.keySet().iterator();
+		while (ite.hasNext()) {
+			String resURL = ite.next();
+			RequestMatcher requestMatcher = new AntPathRequestMatcher(resURL);
+			if (requestMatcher.matches(request)) {
+				Collection<ConfigAttribute> returnCollection = resourceMap.get(resURL);
+				return returnCollection;
+			}
+		}
+		return null;
+	}
 
-	@Override
-	public void afterPropertiesSet() throws Exception {
-		loadResourceDefine();		
+	public boolean supports(Class<?> clazz) {
+		return true;
+	}
+
+	public Collection<ConfigAttribute> getAllConfigAttributes() {
+		return null;
 	}
 }
