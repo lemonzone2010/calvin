402dd1dabef9266ed6dbb453023b642429a46781
hsiayong <hsiayong@gmail.com>
2012-07-03 09:54:43 +0000

    hibernate search fields getted

--- project/beer/application/framework-core/src/main/java/com/apusic/ebiz/framework/core/solr/HibernateEventWiring.java
+++ project/beer/application/framework-core/src/main/java/com/apusic/ebiz/framework/core/solr/HibernateEventWiring.java
@@ -1,9 +1,12 @@
 package com.apusic.ebiz.framework.core.solr;
 
+import java.util.concurrent.Callable;
+
 import javax.annotation.PostConstruct;
 
 import org.hibernate.HibernateException;
 import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
 import org.hibernate.event.service.spi.DuplicationStrategy;
 import org.hibernate.event.service.spi.EventListenerRegistry;
 import org.hibernate.event.spi.EventType;
@@ -27,8 +30,19 @@ import org.hibernate.integrator.spi.IntegratorService;
 import org.hibernate.internal.SessionFactoryImpl;
 import org.hibernate.search.util.logging.impl.Log;
 import org.hibernate.search.util.logging.impl.LoggerFactory;
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.InitializingBean;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+import org.springframework.orm.hibernate3.annotation.AnnotationSessionFactoryBean;
+import org.springframework.orm.hibernate4.LocalSessionFactoryBean;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.transaction.support.TransactionTemplate;
+
+import com.apusic.ebiz.framework.core.util.TransactionHelper;
 
 /**
  * Integrates Hibernate Search into Hibernate Core by registering its needed listeners
@@ -38,14 +52,21 @@ import org.springframework.stereotype.Component;
  * @author Steve Ebersole
  */
 @Component
-public class HibernateEventWiring {
+public class HibernateEventWiring implements InitializingBean{
 
 	private static final Log log = LoggerFactory.make();
 	public static final String AUTO_REGISTER = "hibernate.search.autoregister_listeners";
+	private static Configuration configuration;
 
 	private FullTextIndexEventListener listener;
 	@Autowired
 	private SessionFactory sessionFactory;
+	private TransactionTemplate transactionTemplate;
+
+	@Autowired
+	public void setTransactionManager(PlatformTransactionManager transactionManager) {
+		this.transactionTemplate = new TransactionTemplate(transactionManager);
+	}
 
 	@PostConstruct
 	public void integrate() {
@@ -68,6 +89,17 @@ public class HibernateEventWiring {
 		
 		IntegratorService integratorService = ((SessionFactoryImpl) sessionFactory).getServiceRegistry().getService(
 				IntegratorService.class);
+		
+		TransactionHelper.doInTransaction(transactionTemplate, new Callable<Object>() {
+
+			@Override
+			public Object call() throws Exception {
+				sessionFactory.getCurrentSession();
+				System.out.println("++++++++++++++++++++++++++++++++++");
+				return null;
+				
+			}
+		});
 
 	}
 
@@ -146,5 +178,29 @@ public class HibernateEventWiring {
 		}
 		
 	}
+
+	@Override
+	public void afterPropertiesSet() throws Exception {
+		try {
+			TransactionHelper.doInTransaction(transactionTemplate, new Callable<Object>() {
+
+				@Override
+				public Object call() throws Exception {
+					DocumentHelper.updateSolrSchema(configuration, sessionFactory.getCurrentSession());
+					return null;
+				}
+			});
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+
+	public static void setConfiguration(Configuration configuration) {
+		HibernateEventWiring.configuration = configuration;
+	}
+
+
 
 }
