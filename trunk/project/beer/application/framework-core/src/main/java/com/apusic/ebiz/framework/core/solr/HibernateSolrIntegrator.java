bb742bacc66be4812f7eb97876e38e8d701a5126
hsiayong <hsiayong@96a9805f-92f8-c556-2e62-a92eab900872>
2012-07-03 15:13:16 +0000

    hibernate solr impl,hibernate:side

--- trunk/project/beer/application/framework-core/src/main/java/com/apusic/ebiz/framework/core/solr/HibernateSolrIntegrator.java
+++ trunk/project/beer/application/framework-core/src/main/java/com/apusic/ebiz/framework/core/solr/HibernateSolrIntegrator.java
@@ -5,21 +5,40 @@ import java.util.Iterator;
 import org.hibernate.Session;
 import org.hibernate.cfg.Configuration;
 import org.hibernate.engine.spi.SessionFactoryImplementor;
+import org.hibernate.event.service.spi.EventListenerRegistry;
+import org.hibernate.event.spi.EventType;
 import org.hibernate.integrator.spi.Integrator;
+import org.hibernate.internal.SessionFactoryImpl;
 import org.hibernate.mapping.PersistentClass;
 import org.hibernate.metamodel.source.MetadataImplementor;
 import org.hibernate.service.spi.SessionFactoryServiceRegistry;
 import org.springframework.transaction.annotation.Transactional;
 
 public class HibernateSolrIntegrator implements Integrator {
+	private FullTextIndexEventListener listener;
 	@Override
 	@SuppressWarnings("unchecked")
 	public void integrate(Configuration configuration, SessionFactoryImplementor sessionFactory,
 			SessionFactoryServiceRegistry serviceRegistry) {
-		HibernateEventWiring.setConfiguration(configuration);
-		//updateSolrSchema(configuration, sessionFactory);
+		SolrEventInitalize.setConfiguration(configuration);
 
-		// generateSolrMapping(configuration);
+
+		listener = new FullTextIndexEventListener(  );
+
+		EventListenerRegistry listenerRegistry = ((SessionFactoryImpl) sessionFactory).getServiceRegistry().getService(
+				EventListenerRegistry.class);
+		//TODO if the event is duplicated, do not initialize the newly created listener
+		listenerRegistry.addDuplicationStrategy( new DuplicationStrategyImpl( FullTextIndexEventListener.class ) );
+		listenerRegistry.getEventListenerGroup( EventType.POST_INSERT ).appendListener( listener );
+		listenerRegistry.getEventListenerGroup( EventType.POST_UPDATE ).appendListener( listener );
+		listenerRegistry.getEventListenerGroup( EventType.POST_DELETE ).appendListener( listener );
+		listenerRegistry.getEventListenerGroup( EventType.POST_COLLECTION_RECREATE ).appendListener( listener );
+		listenerRegistry.getEventListenerGroup( EventType.POST_COLLECTION_REMOVE ).appendListener( listener );
+		listenerRegistry.getEventListenerGroup( EventType.POST_COLLECTION_UPDATE ).appendListener( listener );
+		listenerRegistry.getEventListenerGroup( EventType.FLUSH ).appendListener( listener );
+		//listenerRegistry.getEventListenerGroup( EventType.LOAD ).appendListener( listener );
+		
+		
 	}
 
 
