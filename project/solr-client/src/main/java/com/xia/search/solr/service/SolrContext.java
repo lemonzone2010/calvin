4854f86dcd1184e30e6ff69226c6ad29206febb9
hsiayong <hsiayong@gmail.com>
2012-07-06 09:43:07 +0000

    refact

--- project/solr-client/src/main/java/com/xia/search/solr/service/SolrContext.java
+++ project/solr-client/src/main/java/com/xia/search/solr/service/SolrContext.java
@@ -3,6 +3,7 @@ package com.xia.search.solr.service;
 import com.xia.search.solr.hibernate.HibernateContext;
 
 //TODO 配置文件,spring文件
+//TODO 服务器的翻译
 public class SolrContext {
 	public static final String SOLR_SERVER_URL = "http://localhost:8080/solr-server";
 	public static final boolean NEED_UPDATE_SCHEMA_AT_START = true;//启动时，会更新schema.xml，如果已有schema，可以关闭此配置
